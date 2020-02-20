f9c7d44a1c87850fdf6c0844a260963b98f90736
Jessica Liang <jessicursliang@gmail.com>
2020-02-20 11:31:42 -0800

    Developed method for converting BINARY to DECIMAL.

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,13 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal = 0
+
+  binary_array.length.times do |i|
+    decimal += binary_array[i] * (2 ** (binary_array.length - (i + 1)))
+  end
+
+  return decimal
 end
