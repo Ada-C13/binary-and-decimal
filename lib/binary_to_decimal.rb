e9c81201043cc65745bde5c2536fb22161dd21f1
Catherina <catherine.pchelina@gmail.com>
2020-02-21 18:57:55 -0800

    Implemented binary_to_decimal method.

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,33 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
+def generate_binary_num
+  binary_num = []
+  8.times do
+    binary_num << rand(0..1)
+  end
+
+  return binary_num
+end
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  if binary_array.length == 0
+    raise ArgumentError.new, "Array cannot be empty "
+  end
+
+  decimal = 0
+
+  power = binary_array.length - 1
+
+  binary_array.length.times do |i|
+    if binary_array[i] == 1
+      decimal += 2 ** power
+      power -= 1
+    else
+      power -= 1
+    end
+  end
+
+  return decimal
 end
