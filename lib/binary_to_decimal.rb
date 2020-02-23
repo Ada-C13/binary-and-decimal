50c0b98878c8680e1702fc73e8e7944250db7238
denisseai <msdenisseanaya@gmail.com>
2020-02-23 14:13:01 -0800

    first commit - working

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -2,8 +2,16 @@
 # The array is randomly filled with 0’s and 1’s.
 # The most significant bit is at index 0.
 # The least significant bit is at index 7.
+
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal_value = []
+  binary_array.each_with_index do |num, index|
+    exponent = ((binary_array.length - 1) - index)
+    if num == 1
+      decimal_value << 2 ** exponent
+    end
+  end
+  return decimal_value.sum
 end
