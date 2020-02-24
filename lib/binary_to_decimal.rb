f591650cb0382141f89dc10c01a7aef76b7567c6
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-02-23 23:15:05 -0800

    Created binary_to_decimal method and passed the tests.

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,64 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
-def binary_to_decimal(binary_array)
-  raise NotImplementedError
-end
+# def binary_to_decimal(binary_array)
+#   sum = 0
+#   index = 0
+#   while index < binary_array.length
+#     if binary_array[index] == 1
+#       decimal = 2**(binary_array.length - (index+1)) 
+#     else 
+#       decimal = 0
+#     end
+#       sum += decimal
+#       index +=1 
+#   end
+#   return sum
+# end
+  
+
+# array = [1, 0, 0, 1, 1, 0, 0, 1]
+# puts binary_to_decimal(array)
+
+###########Using each_with_index
+
+# def binary_to_decimal(binary_array)
+#   sum  = 0 
+#   binary_array.each_with_index do |element, index|
+#     if element == 1
+#       decimal_element = 2**(binary_array.length - (index+1))
+#     else 
+#       decimal_element = 0 #return 0 if the element = 0
+#     end
+#     sum += decimal_element
+#   end
+#   return sum
+# end
+
+#############Using .each 
+# def binary_to_decimal(binary_array)
+#   sum = 0
+#   index = 0
+#   binary_array.each do |element|  
+#     if element == 1
+#       decimal_element = 2**(binary_array.length - (index+1))
+#     else
+#       decimal_element = 0
+#     end 
+#     sum += decimal_element
+#     index +=1 
+#   end
+#   return sum    
+#   end
+
+ 
+
+
+
+
+
+
+
+
+  
+
