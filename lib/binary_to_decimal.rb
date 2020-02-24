a6a1ea1c712d813342324c8ba9fcadaa2ce2b6dc
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-02-23 18:01:48 -0800

    binary_to_decimal methods

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -3,7 +3,35 @@
 # The most significant bit is at index 0.
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
-# the algorithm you devised in class.
+# # the algorithm you devised in class.
+
+
+
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  raise ArgumentErrot if binary_array == nil
+  decimal = 0
+  index = 0
+  while index < binary_array.length
+    decimal += binary_array[index] * 2 ** (binary_array.length - 1 - index)
+    index += 1
+  end
+  return decimal
 end
+
+dec  = binary_to_decimal([1, 0, 0, 1, 1, 0, 1, 0])
+print dec
+
+
+
+def binary_to_decimal(binary_array)
+  raise ArgumentErrot if binary_array == nil
+  decimal  = 0
+  binary_array.each_with_index do |digit, index|
+    decimal += digit * 2 ** (binary_array.length - 1 - index)
+  end
+  return decimal
+end
+
+dec  = binary_to_decimal([1, 0, 0, 1, 1, 0, 1, 0])
+print dec
