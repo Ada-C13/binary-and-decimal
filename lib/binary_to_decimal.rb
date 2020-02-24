9c4ff05d979f614f6b5d00b56f1856a36e869d61
Rosalyn <syrosalynyu@gmail.com>
2020-02-23 17:53:04 -0800

    Completed both binary_to_decimal and decimal_to_binary PLUS three minitests for the decimal_to_binary method

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,36 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  x = 0 # index of the most significant bit
+  y = binary_array.length - 1 # index of the least significant bit
+  while x < y
+    temp = binary_array[x]
+    binary_array[x] = binary_array[y]
+    binary_array[y] = temp
+    x += 1
+    y -= 1
+  end
+  num = (binary_array[0] * 2**0) +
+        (binary_array[1] * 2**1) +
+        (binary_array[2] * 2**2) +
+        (binary_array[3] * 2**3) +
+        (binary_array[4] * 2**4) +
+        (binary_array[5] * 2**5) +
+        (binary_array[6] * 2**6) +
+        (binary_array[7] * 2**7)
+  return num
+end
+
+def decimal_to_binary(decimal_num)
+  binary_array = []
+  until decimal_num == 0
+    if decimal_num % 2 == 1
+      binary_array << 1
+    elsif decimal_num % 2 == 0
+      binary_array << 0
+    end
+  decimal_num /= 2
+  end
+  binary_num = binary_array.reverse.join.to_i 
+  return binary_num
 end
